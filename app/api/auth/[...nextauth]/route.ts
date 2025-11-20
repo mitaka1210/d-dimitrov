9abd05f35850f1ea8fea7e18875225480220a8f2
Dimitar <dimitard185@gmail.com>
2025-11-20 15:48:33 +0200

    feature/MB-30 Refactor NextAuth config and update tsconfig formatting
    feature/MB-30 Refactored NextAuth configuration to use NEXTAUTH_SECRET and separated options from handler for clarity. Updated tsconfig.json formatting for improved readability. Dependency updates in package-lock.json.

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,14 +1,16 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
-const handler = NextAuth({
+const authOptions = {
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID ?? "",
       clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
     }),
   ],
-  secret: process.env.GOOGLE_CLIENT_SECRET,
-});
+  secret: process.env.NEXTAUTH_SECRET,
+};
 
-export {handler as GET, handler as POST};
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
